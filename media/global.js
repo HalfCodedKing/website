4e4299588cc63d8d3431566e63aabb709f14fbbd
Blobtoe <felixxperron@gmail.com>
2020-06-14 01:48:52 -0700

    made header look extra spicy

--- media/global.js
+++ media/global.js
@@ -6,16 +6,25 @@ header = `
         <li><a name="projects" href="/projects/index.html">Projects</a></li>
         <li><a name="home" href="/index.html">Home</a></li>
     </ul>
+    <div id="title">
+        <h1></h1>
+    </div>
 </div>
 
 <style>
     #menu {
-        margin-top: 20px;
-        margin-right: 20px;
+        text-align: center;
+        padding: 0;
+        margin: 0;
+        position: relative;
+        height: 200px;
         background-color: #2b2b2b;
     }
     
     #menu ul {
+        position: absolute;
+        top: 20px;
+        right: 20px;
         list-style-type: none;
         overflow: hidden;
         margin: 0;
@@ -36,6 +45,18 @@ header = `
 
     #menu ul li a:hover {
         color: rgb(161, 161, 161);
+    }
+
+    #title {
+        padding-top: 90px;
+    }
+
+    #title h1 {
+        font-family: 'Titillium Web', sans-serif;
+        color: whitesmoke;
+        font-size: 60px;
+        border-bottom: 5px solid red;
+        display: inline;
     }
 </style>
 
