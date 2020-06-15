f687b8ab1a39771f89e375288571072578ca78ee
Felix Perron <felixxperron@gmail.com>
2020-06-15 12:15:20 -0700

    auto commit

--- commit.sh
+++ commit.sh
@@ -1,4 +1,5 @@
 git -C /home/pi/website/ fetch origin master
+git -C /home/pi/website/ pull origin master
 git -C /home/pi/website/ add --all
 git -C /home/pi/website/ commit -m "auto commit"
 git -C /home/pi/website/ push origin master
