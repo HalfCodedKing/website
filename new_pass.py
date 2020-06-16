41dd68a3c976e6554453ab96d99f76bff60f60a0
Blobtoe <felixxperron@gmail.com>
2020-06-15 21:04:09 -0700

    fixed path problems

--- new_pass.py
+++ new_pass.py
@@ -3,15 +3,17 @@ import sys
 import re
 
 if __name__ == "__main__":
-    title = sys.argv[1]
-    main_image = sys.argv[2]
-    a = sys.argv[3]
-    b = sys.argv[4]
-    msa = sys.argv[5]
-    raw = sys.argv[6]
+    now = sys.argv[1]
+    title = sys.argv[2]
+    main_image = sys.argv[3]
+    a = sys.argv[4]
+    b = sys.argv[5]
+    msa = sys.argv[6]
+    raw = sys.argv[7]
     html = open("/home/pi/website/media/pass.html")
     src = Template(html.read())
-    d = {"title":title, "main":main_image, "a":a, "b":b, "msa":msa, "raw":raw}
+    prefix = "/weather/images/{}/".format(now)
+    d = {"title":title, "main":prefix + main_image.split("/")[-1], "a":prefix + a.split("/")[-1], "b":prefix + b.split("/")[-1], "msa":prefix + msa.split("/")[-1], "raw":prefix + raw.split("/")[-1]}
     result = src.substitute(d)
 
     html = open("/home/pi/website/weather/index.html", "r")
