7e4fcd24b8e5414f5fd8872aac7554cacb71285e
Blobtoe <felixxperron@gmail.com>
2020-07-06 20:56:04 -0700

    changed gain to 49.6

--- weather/scripts/process.py
+++ weather/scripts/process.py
@@ -48,7 +48,7 @@ def upload(path, title):
 def process_METEOR():
     #record pass
     print("recording pass...")
-    os.system("timeout {} /usr/local/bin/rtl_fm -Mraw -s768k -f {} -g37.2 -p 0 | sox -t raw -r 768k -c 2 -b 16 -e s - -t wav {}.iq.wav rate 192k".format(duration, frequency, outfile))
+    os.system("timeout {} /usr/local/bin/rtl_fm -Mraw -s768k -f {} -g49.6 -p 0 | sox -t raw -r 768k -c 2 -b 16 -e s - -t wav {}.iq.wav rate 192k".format(duration, frequency, outfile))
 
     #demodulate the signal
     print("demodulating meteor signal...")
